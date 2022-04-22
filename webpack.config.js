1c11b5f270fd4fa7d86e03828aca9f00f2489e3a
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2022-04-22 18:32:58 +0100

    Added file-loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
 };
