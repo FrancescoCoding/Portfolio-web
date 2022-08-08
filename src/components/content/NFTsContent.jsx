f1f642c04650435343b0dfe415b2730a97dc9bac
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2022-08-08 11:52:38 +0100

    Update v0.95.06 [API test]

--- src/components/content/NFTsContent.jsx
+++ src/components/content/NFTsContent.jsx
@@ -2,9 +2,15 @@ import styles from "./NFTsContent.module.css";
 import { useEffect, useState } from "react";
 
 const NFTsContent = () => {
+  const [isMount, setIsMount] = useState(true);
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
+    if (isMount) {
+      setIsMount(false);
+      return;
+    }
+
     fetch("https://francesco-api.herokuapp.com/projects")
       .then(response => response.json())
       .then(data => {
