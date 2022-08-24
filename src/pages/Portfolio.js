ee5f7b3f134f240292ea162fc9e682abcd6dbcdc
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2022-08-24 10:52:24 +0100

    Update v0.95.11 [Performance test]

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -11,7 +11,7 @@ const Portfolio = () => {
       <Navbar />
       <PortfolioContent astronautDelaySec={"1s"} />
       <ColourSelector />
-      <Sidebar />
+      {/* <Sidebar /> */}
     </>
   );
 };
