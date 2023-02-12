67cdadfae0a2b779871a74e42dd1d60acfec7f6e
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2023-02-12 01:51:04 +0000

    Security update, nested quantifiers from regex

--- src/components/UI/ContactMeForm.jsx
+++ src/components/UI/ContactMeForm.jsx
@@ -5,9 +5,7 @@ import { useSelector } from "react-redux";
 import { Success } from "../../assets/Animations/Animations";
 
 const validateEmail = email => {
-  const regex =
-    // eslint-disable-next-line no-useless-escape
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
   return regex.test(String(email).toLowerCase());
 };
 
