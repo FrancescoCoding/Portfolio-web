f7b94fe3786cb39ffd5d20a09e3d94922088ed03
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2023-10-22 18:02:20 +0100

    Update WorksList.js

--- src/settings/WorksList.js
+++ src/settings/WorksList.js
@@ -560,7 +560,7 @@ const PROJECTS = [
         "Chatbot Rick Sanchez full-stack potenziato da ChatGPT. Backend in Node/Express e Frontend in React 18 (Vite).",
     },
     id: "p12",
-    githubLink: "https://github.com/FrancescoCoding/ask-Sanchez",
+    githubLink: "https://ask-sanchez-web.vercel.app/",
     image:
       "https://images.unsplash.com/photo-1681460590033-67b0d1413550?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
     endpoint: "ask-sanchez",
