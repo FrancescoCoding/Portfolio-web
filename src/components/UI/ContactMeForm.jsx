53dd6a4c22d6f935c173f432efa3b371f1e513bd
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2023-11-12 21:50:30 +0000

    Added hashes to about/form & about/info, refactored form to Formik with Yup

--- src/components/UI/ContactMeForm.jsx
+++ src/components/UI/ContactMeForm.jsx
@@ -3,14 +3,23 @@ import emailjs from "emailjs-com";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import { useSelector } from "react-redux";
-import { TextField, Button, Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import { Success } from "../../assets/Animations/Animations";
+
+import styles from "./ContactMeForm.module.css";
 
 // Validation schema for the form
 const ContactSchema = Yup.object().shape({
-  name: Yup.string().required("Name is required").max(50, "Too long!"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
-  message: Yup.string().required("Message is required"),
+  name: Yup.string()
+    .required("Name is required 🤭")
+    .max(50, "Your name is a bit too long! 👀"),
+  email: Yup.string()
+    .email("Invalid email 🤔")
+    .required("Email is required 🤭"),
+  message: Yup.string().required("Message is required 🤭"),
 });
 
 export const ContactMeForm = () => {
@@ -20,43 +29,34 @@ export const ContactMeForm = () => {
   const isEnglish = storeLanguage === "EN";
   const [emailSent, setEmailSent] = useState(false);
 
-  const sendEmail = (values, { resetForm }) => {
+  const sendEmail = (values, { setSubmitting, resetForm }) => {
+    setSubmitting(true);
     emailjs
-      .sendForm(
+      .send(
         "service_wp9ydnd",
         "template_28ozq9m",
-        form.current,
+        values,
         "user_3A2DqTXIldcHsNOG17QYb"
       )
       .then(
         (result) => {
-          console.log(values.text);
           setEmailSent(true);
           setTimeout(() => setEmailSent(false), 4000);
           resetForm();
         },
         (error) => {
-          console.log(error.text);
+          console.error(error.text);
         }
-      );
+      )
+      .finally(() => setSubmitting(false));
   };
+
   const textFieldSx = {
     input: {
       color: storeColour.hex, // Use dynamic color from the state
-      backgroundColor: "rgba(255, 255, 255, 0.1)", // Input background color
     },
     "& label.Mui-focused": {
       color: storeColour, // Label color when focused
-    },
-    // also when the user clicks on the input or types in it
-
-    "& .MuiInputLabel-root": {
-      // Add enough space for the label
-      padding: "0 5px",
-      background: "var(--input-background)", // Match the input's background
-      "&.Mui-focused": {
-        color: "var(--label-color-focused)", // Label color when the input is focused
-      },
     },
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
@@ -64,116 +64,154 @@ export const ContactMeForm = () => {
       },
       // do same for textarea
       "& textarea": {
-        borderColor: `var(--${storeColour.colour}-active)`, // Use CSS variable for the border
         shrink: true,
+        color: `var(--${storeColour.colour}-primary-color)`, // Use the CSS variable for text color
       },
-      "& input:-webkit-autofill": {},
+      "& input:-webkit-autofill": {
+        WebkitBoxShadow: `0 0 0 1000px var(--input-background) inset`,
+        WebkitTextFillColor: `var(--${storeColour.colour}-primary-color)`,
+      },
       "&:hover fieldset": {
         borderColor: `var(--${storeColour.colour}-active)`, // Border color when hovered
       },
       "&.Mui-focused fieldset": {
-        borderColor: storeColour, // Border color when focused
-      },
-      "&.Mui-error fieldset": {
-        borderColor: "red", // Border color when there is an error
+        borderColor: storeColour.hex, // Border color when focused
       },
     },
     "& .MuiFormHelperText-root.Mui-error": {
-      color: "red", // Error message color
-      fontSize: "13px", // Adjust the font-size as needed
+      color: "var(--error-color)",
+      fontSize: "17px",
       margin: 0,
+      fontWeight: 600,
     },
-
     width: "100%", // Full width
-    mb: 2, // Margin bottom
+    mb: 2,
   };
 
   return (
-    <div>
-      {!emailSent ? (
-        <Formik
-          initialValues={{ name: "", email: "", message: "" }}
-          validationSchema={ContactSchema}
-          onSubmit={sendEmail}
-          innerRef={form}>
-          {({ errors, touched }) => (
-            <Form>
-              <Field
-                as={TextField}
-                name="name"
-                label={isEnglish ? "Name" : "Nome"}
-                error={touched.name && !!errors.name}
-                helperText={touched.name && errors.name}
-                sx={textFieldSx}
-                InputLabelProps={{
-                  style: {
-                    color: touched.name
-                      ? storeColour
-                      : "rgba(255, 255, 255, 0.7)",
-                  },
-                  shrink: true,
-                }}
-              />
-              <Field
-                as={TextField}
-                type="email"
-                name="email"
-                label="Email"
-                error={touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
-                sx={textFieldSx}
-                InputLabelProps={{
-                  style: {
-                    color: touched.email
-                      ? storeColour
-                      : "rgba(255, 255, 255, 0.7)",
-                  },
-                  shrink: true,
-                }}
-              />
-              <Field
-                as={TextField}
-                name="message"
-                label={isEnglish ? "Message" : "Messaggio"}
-                multiline
-                rows={4}
-                error={touched.message && !!errors.message}
-                helperText={touched.message && errors.message}
-                sx={{
-                  ...textFieldSx, // Include other styles you have defined
-                  "& .MuiInputBase-inputMultiline": {
-                    color: `var(--${storeColour.colour}-primary-color)`, // Use the CSS variable for text color
-                  },
-                  // Include other styles for borders and such
-                }}
-                InputLabelProps={{
-                  style: {
-                    color: touched.message
-                      ? `var(--${storeColour.colour}-active)`
-                      : "rgba(255, 255, 255, 0.7)",
-                  },
-                  shrink: true,
-                }}
-              />
-              <Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 2, bgcolor: storeColour.hex }}>
-                Send
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      ) : (
-        <div>
-          <Success />
-          <p>
+    <Box className={styles.box}>
+      {!emailSent && (
+        <>
+          {" "}
+          <div className={styles.greetingMessage}>
             {isEnglish
-              ? "Your message has been sent! I will get back to you as soon as possible."
-              : "Il tuo messaggio è stato inviato! Ti risponderò il prima possibile."}
-          </p>
+              ? "Feel free to drop me a message using the form below!"
+              : "Sentiti libero di mandarmi un messaggio qui sotto!"}
+          </div>
+          <Formik
+            initialValues={{ name: "", email: "", message: "" }}
+            validationSchema={ContactSchema}
+            onSubmit={sendEmail}
+            innerRef={form}>
+            {({ errors, touched, isValid, isSubmitting }) => (
+              <Form>
+                <Grid
+                  container
+                  spacing={2}>
+                  <Grid item>
+                    <Field
+                      as={TextField}
+                      name="name"
+                      label={isEnglish ? "Name" : "Nome"}
+                      error={touched.name && !!errors.name}
+                      helperText={touched.name && errors.name}
+                      required
+                      sx={textFieldSx}
+                      InputLabelProps={{
+                        style: {
+                          color: "rgba(255, 255, 255, 0.7)",
+                        },
+                        shrink: true,
+                      }}
+                    />
+                    <Field
+                      as={TextField}
+                      type="email"
+                      name="email"
+                      label="Email"
+                      required
+                      error={touched.email && !!errors.email}
+                      helperText={touched.email && errors.email}
+                      sx={textFieldSx}
+                      InputLabelProps={{
+                        style: {
+                          color: "rgba(255, 255, 255, 0.7)",
+                        },
+                        shrink: true,
+                      }}
+                    />
+                    <Field
+                      as={TextField}
+                      name="message"
+                      label={isEnglish ? "Message" : "Messaggio"}
+                      multiline
+                      rows={4}
+                      required
+                      error={touched.message && !!errors.message}
+                      helperText={touched.message && errors.message}
+                      sx={{
+                        ...textFieldSx,
+                        "& .MuiInputBase-inputMultiline": {},
+                      }}
+                      InputProps={{
+                        style: {
+                          "::placeholder": {
+                            color: "rgba(255, 255, 255, 0.7)",
+                          },
+                        },
+                      }}
+                      InputLabelProps={{
+                        style: {
+                          color: "rgba(255, 255, 255, 0.7)",
+                        },
+                      }}
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    style={{
+                      position: "absolute",
+                      right: 0,
+                      bottom: 0,
+                    }}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      disabled={!isValid || isSubmitting}
+                      sx={{
+                        bgcolor: `var(--${storeColour.colour}-active) !important`,
+                        width: "150px !important",
+                      }}>
+                      {isSubmitting && isEnglish && "Sending..."}
+                      {isSubmitting && !isEnglish && "Invio..."}
+                      {!isSubmitting && isEnglish && "Send"}
+                      {!isSubmitting && !isEnglish && "Invia"}
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Form>
+            )}
+          </Formik>
+        </>
+      )}
+      {emailSent && (
+        <div className={styles["success-container"]}>
+          <Success />
+
+          {isEnglish && (
+            <p className={styles.success}>
+              Your message has been sent! <br /> I will get back to you as soon
+              as possible.{" "}
+            </p>
+          )}
+          {!isEnglish && (
+            <p className={styles.success}>
+              Il tuo messaggio è stato inviato! <br /> Ti risponderò il prima
+              possibile.{" "}
+            </p>
+          )}
         </div>
       )}
-    </div>
+    </Box>
   );
 };
